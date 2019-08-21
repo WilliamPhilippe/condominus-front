432bbfbd6b8e18a162a20522d6e577da82aaa74e
William Philippe <wplb@ic.ufal.br>
2019-08-21 16:13:48 -0300

    up

---
+++ build/precache-manifest.9e2f607c2933a35f3c127c9b4632ca04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1b0039852c315b6161ce46d36b4657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b7789994c1322ecfca2",
+    "url": "/static/css/main.5f3ac91a.chunk.css"
+  },
+  {
+    "revision": "79ad7fd334805bc7743f",
+    "url": "/static/js/2.ec27fbb6.chunk.js"
+  },
+  {
+    "revision": "1b7789994c1322ecfca2",
+    "url": "/static/js/main.818f8135.chunk.js"
+  },
+  {
+    "revision": "219707186523ad4b8cde",
+    "url": "/static/js/runtime~main.821e3928.js"
+  }
+]);
